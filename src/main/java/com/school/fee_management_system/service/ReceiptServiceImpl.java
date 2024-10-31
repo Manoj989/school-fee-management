9d79a882662cc64f19c589b8178544ba4e23112b
Manoj <manojhota9897@gmail.com>
2024-11-01 02:45:07 +0530

    changes

--- src/main/java/com/school/fee_management_system/service/ReceiptServiceImpl.java
+++ src/main/java/com/school/fee_management_system/service/ReceiptServiceImpl.java
@@ -1,5 +1,6 @@
 package com.school.fee_management_system.service;
 
+import com.school.fee_management_system.Exception.ResourceNotFoundException;
 import com.school.fee_management_system.model.Receipt;
 import com.school.fee_management_system.repository.ReceiptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ public class ReceiptServiceImpl implements ReceiptService{
 
     @Override
     public Receipt getReceiptByOrderId(String orderId) {
+        if(receiptRepository.findByOrderId(orderId)==null)
+            throw new ResourceNotFoundException("Receipt Details Not Found");
         return receiptRepository.findByOrderId(orderId);
     }
 }
